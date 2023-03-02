74b16bcba7cbae5b0867563e192da961174adf97
Vadx <lv_km@ukr.net>
2023-03-02 18:22:30 +0200

    add framer-motion, css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 
+import '../styles/tailwind.css'
+import '../styles/app.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={false} disableTransitionOnChange>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
